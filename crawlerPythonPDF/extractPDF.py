6137151767307e31ea0e4bcede9ef380f72c2f8e
Gabriel Reis Miranda <grmiranda97@gmail.com>
2019-11-21 10:03:53 -0300

    atualizando parser do PDF

--- crawlerPythonPDF/extractPDF.py
+++ crawlerPythonPDF/extractPDF.py
@@ -1,15 +1,55 @@
+import numpy as np
 import tabula
 import pandas as pd
-# Read pdf into DataFrame
-# PODE USAR O LINK ABAIXO COMO FONTE DE INICIAL PARA ENTENDER AS VARIÁVEIS USADAS.
-# https://aegis4048.github.io/parse-pdf-files-while-retaining-structure-with-tabula-py
+
+# UTIL Functions
+def is_nan(x):
+    return (x is np.nan or x != x)
+# ======================================================================
+
 useLattice = False
 useStream  = True
-usePages   = '2' #coloca o número da página ex '2' ou 'all' para pegar todas as tabelas da página
-df = tabula.read_pdf("congeladores.pdf", multiple_tables=True, lattice=useLattice, stream=useStream, pages=usePages)
+usePages   = 'all' #coloca o número da página ex '2' ou 'all' para pegar todas as tabelas da página
 
-# Também é possível ler um pdf direto de um link
-# df = tabula.read_pdf("https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")
-print(df)
-# convert PDF into CSV
-# tabula.convert_into("congeladores.pdf", "output.csv", output_format="csv", pages='all')
+
+dataArray = []
+
+dataArray.append('============================================================================================================================')
+
+df = tabula.read_pdf("televisores.pdf", multiple_tables=True, lattice=True, stream=False, pages='all')
+for table in df:
+    data = pd.DataFrame(table) #convertendo pagina em Panda DataFrame
+    for row, value in data.iterrows():
+        rowData = []
+        hasNaN = False
+        for collumn in list(data):
+            if is_nan(data[collumn][row]) :
+                hasNaN = True
+            else:
+                rowData.append(data[collumn][row])
+        if not hasNaN:
+            dataArray.append(rowData)
+
+dataArray.append('============================================================================================================================')
+
+df = tabula.read_pdf("congeladores.pdf", multiple_tables=True, lattice=False, stream=True, pages='all')
+for table in df:
+    data = pd.DataFrame(table) #convertendo pagina em Panda DataFrame
+    for row, value in data.iterrows():
+        rowData = []
+        hasNaN = False
+        for collumn in list(data):
+            if is_nan(data[collumn][row]) :
+                hasNaN = True
+            else:
+                rowData.append(data[collumn][row])
+        if not hasNaN:
+            dataArray.append(rowData)
+
+dataArray.append('============================================================================================================================')
+
+
+
+
+for x in dataArray:
+    print(x)
