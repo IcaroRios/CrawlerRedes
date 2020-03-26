6c1bc4c86e43f3c6f3db5fcf5c782a3e20a762c7
Ícaro Cardoso Rios <rockicarorios@gmail.com>
2020-03-26 17:18:11 -0300

    voltando

--- crawlerPythonPDF/extractPDF.py
+++ crawlerPythonPDF/extractPDF.py
@@ -6,23 +6,20 @@ import json
 def is_nan(x):
     return (x is np.nan or x != x)
 # ======================================================================
-def lambda_handler(event, context):
-    dataArray = []
-    df = tabula.read_pdf("http://www.inmetro.gov.br/consumidor/pbe/congeladores.pdf", multiple_tables=True, lattice=False, stream=True, pages='all')
-    for table in df:
-        data = pd.DataFrame(table) #convertendo pagina em Panda DataFrame
-        for row, value in data.iterrows():
-            rowData = []
-            hasNaN = False
-            for collumn in list(data):
-                if is_nan(data[collumn][row]) :
-                    hasNaN = True
-                else:
-                    rowData.append(data[collumn][row])
-            if not hasNaN:
-                dataArray.append(rowData)
-    return {
-        'statusCode': 200,
-        'body': json.dumps(dataArray)
-    }
+dataArray = []
+df = tabula.read_pdf("http://www.inmetro.gov.br/consumidor/pbe/congeladores.pdf", multiple_tables=True, lattice=False, stream=True, pages='all')
+for table in df:
+    data = pd.DataFrame(table) #convertendo pagina em Panda DataFrame
+    for row, value in data.iterrows():
+        rowData = []
+        hasNaN = False
+        for collumn in list(data):
+            if is_nan(data[collumn][row]) :
+                hasNaN = True
+            else:
+                rowData.append(data[collumn][row])
+        if not hasNaN:
+            dataArray.append(rowData)
+
+print(dataArray)
 
