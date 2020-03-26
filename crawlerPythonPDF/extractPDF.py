7338838ec6b27b9f3ffa73ad8f0e18083e2a588a
Ícaro Cardoso Rios <rockicarorios@gmail.com>
2020-03-26 16:28:27 -0300

    colocando para retornar json

--- crawlerPythonPDF/extractPDF.py
+++ crawlerPythonPDF/extractPDF.py
@@ -1,30 +1,28 @@
 import numpy as np
 import tabula
 import pandas as pd
-
+import json
 # UTIL Functions
 def is_nan(x):
     return (x is np.nan or x != x)
 # ======================================================================
+def lambda_handler(event, context):
+    dataArray = []
+    df = tabula.read_pdf("http://www.inmetro.gov.br/consumidor/pbe/congeladores.pdf", multiple_tables=True, lattice=False, stream=True, pages='all')
+    for table in df:
+        data = pd.DataFrame(table) #convertendo pagina em Panda DataFrame
+        for row, value in data.iterrows():
+            rowData = []
+            hasNaN = False
+            for collumn in list(data):
+                if is_nan(data[collumn][row]) :
+                    hasNaN = True
+                else:
+                    rowData.append(data[collumn][row])
+            if not hasNaN:
+                dataArray.append(rowData)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(dataArray)
+    }
 
-dataArray = []
-
-dataArray.append('============================================================================================================================')
-df = tabula.read_pdf("http://www.inmetro.gov.br/consumidor/pbe/congeladores.pdf", multiple_tables=True, lattice=False, stream=True, pages='all')
-for table in df:
-    data = pd.DataFrame(table) #convertendo pagina em Panda DataFrame
-    for row, value in data.iterrows():
-        rowData = []
-        hasNaN = False
-        for collumn in list(data):
-            if is_nan(data[collumn][row]) :
-                hasNaN = True
-            else:
-                rowData.append(data[collumn][row])
-        if not hasNaN:
-            dataArray.append(rowData)
-
-dataArray.append('============================================================================================================================')
-
-for x in dataArray:
-    print(x)
