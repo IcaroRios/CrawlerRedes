e894bd45c8bbaf754b40d2ab8847e69ba5e02f2a
Gabriel Reis Miranda <gabriel.dev@golfarma.com.br>
2020-03-30 22:29:06 -0300

    front-end

---
+++ dashboard/src/layouts/MainLayout.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+        <q-toolbar-title>
+          ComparEletro
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
+      <q-list>
+        <q-item clickable tag="a" :to="'/'">
+          <q-item-section avatar><q-icon name="format_list_bulleted" /></q-item-section>
+          <q-item-section><q-item-label>Eletrodomesticos</q-item-label></q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: 'Docs',
+          caption: 'quasar.dev',
+          icon: 'school',
+          link: 'https://quasar.dev'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ dashboard/src/pages/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-page class="flex">
+    <div class="q-pa-md full-width">
+      <q-table
+        title="Congeladores"
+        :data="data"
+        :columns="columns"
+        row-key="name"
+        bordered
+        table-header-class="bg-blue-3"
+        table-bottom-class="bg-blue-3"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      columns: [
+        { name: 'fornecedor', label: 'Fornecedor', field: 'fornecedor' },
+        { name: 'marca', label: 'Marca', field: 'marca' },
+        { name: 'modelo', label: 'Modelo', field: 'modelo' },
+        { name: 'volume', label: 'Volume', field: 'volume' },
+        { name: 'consumo_127', label: 'Consumo 127V', field: 'consumo_127' },
+        { name: 'consumo_220', label: 'Consumo 220V', field: 'consumo_220' },
+        { name: 'eficiencia_127', label: 'Eficiência 127V', field: 'eficiencia_127' },
+        { name: 'eficiencia_220', label: 'Eficiência 220V', field: 'eficiencia_220' },
+        { name: 'classificacao_127', label: 'Classificação 127V', field: 'classificacao_127' },
+        { name: 'classificacao_220', label: 'Classificação 220V', field: 'classificacao_220' }
+      ],
+      data: [
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' },
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' },
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' },
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' },
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' },
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' },
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' },
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' },
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' },
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' },
+        { fornecedor: 'PHILCO', marca: 'PHILCO', modelo: 'CHB42D', volume: 675, consumo_127: 35, consumo_220: 45, eficiencia_127: 0.786, eficiencia_220: 0.746, classificacao_127: 'D', classificacao_220: 'D' }
+      ]
+    }
+  }
+}
+</script>
